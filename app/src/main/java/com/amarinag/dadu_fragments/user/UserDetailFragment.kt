5f441bbc9defe064647c75121afece3fb765f4d5
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-06-01 09:33:10 +0200

    added user fragments

--- app/src/main/java/com/amarinag/dadu_fragments/user/UserDetailFragment.kt
+++ app/src/main/java/com/amarinag/dadu_fragments/user/UserDetailFragment.kt
@@ -15,9 +15,4 @@ class UserDetailFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_user_detail, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
 }
