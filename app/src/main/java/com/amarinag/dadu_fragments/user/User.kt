f5fa5dbb70509d6573b30b123cf9eaa62a13eee0
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-06-01 11:47:09 +0200

    create adapter for users list

---
+++ app/src/main/java/com/amarinag/dadu_fragments/user/User.kt
@@ -0,0 +1,7 @@
+package com.amarinag.dadu_fragments.user
+
+data class User(
+    val id: String,
+    val username: String,
+    val avatarUrl: String
+)
---
+++ app/src/main/java/com/amarinag/dadu_fragments/user/UserAdapter.kt
@@ -0,0 +1,42 @@
+package com.amarinag.dadu_fragments.user
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.amarinag.dadu_fragments.R
+
+class UserAdapter : ListAdapter<User, UserAdapter.ViewHolder>(UserDiffUtilCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view: View = layoutInflater.inflate(R.layout.item_user, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.tvUsername.text = user.username
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvUsername: TextView = view.findViewById(R.id.tv_username)
+        val ivAvatar: ImageView = view.findViewById(R.id.iv_avatar)
+    }
+}
+
+
+class UserDiffUtilCallback : DiffUtil.ItemCallback<User>() {
+    override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+        return oldItem == newItem
+    }
+}
