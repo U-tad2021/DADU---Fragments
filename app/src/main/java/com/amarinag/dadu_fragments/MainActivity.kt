03fcb0f45a4e7483c526f647a6079dcca2e9e866
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-06-01 11:22:51 +0200

    binding bottomnav with navigation

--- app/src/main/java/com/amarinag/dadu_fragments/MainActivity.kt
+++ app/src/main/java/com/amarinag/dadu_fragments/MainActivity.kt
@@ -2,12 +2,19 @@ package com.amarinag.dadu_fragments
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bvn: BottomNavigationView = findViewById(R.id.bnv_main)
+        bvn.setupWithNavController(navController)
     }
 }
